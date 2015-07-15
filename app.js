482e42fb69b3e79c087fdf6e30954590b44091df
Thomas Richner <thomasrichner+github@gmail.com>
2015-07-16 00:26:20 +0200

    cleanup

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var shortener = require('./routes/shortener');
-var users = require('./routes/users');
 
 var app = express();
 
